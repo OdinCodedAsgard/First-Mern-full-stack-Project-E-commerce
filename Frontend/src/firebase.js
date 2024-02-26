db4f3cd92df4fbc818c35fd9e2c80bd27a18d9c3
OdinCodedAsgard <160796898+OdinCodedAsgard@users.noreply.github.com>
2024-02-25 20:21:05 -0500

    Update firebase.js

--- Frontend/src/firebase.js
+++ Frontend/src/firebase.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAEt_zL3KTzr8u8lpdyMBoNMkjqK6OgrTo",
+  apikeyhere;
   authDomain: "mernstate-9a113.firebaseapp.com",
   projectId: "mernstate-9a113",
   storageBucket: "mernstate-9a113.appspot.com",
-  messagingSenderId: "525145165623",
-  appId: "1:525145165623:web:8ac64b379a3d99d68994b2"
+  messagingSenderId here;
+  appId here;
 };
 
 // Initialize Firebase
